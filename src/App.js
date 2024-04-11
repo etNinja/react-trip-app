28004f992df26308769b7acdd485136220592ba1
Andrey Markunas <holyandrewchunk@gmail.com>
2024-04-11 17:09:52 +0300

    update footer component

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function PackingList() {
 }
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you ..asdfk</em>
     </footer>
   );
